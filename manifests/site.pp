0e6918c408e7b9d95c8658329a593abf50209211
Eric Edgar <eedgar@zenoss.com>
2010-08-25 19:10:35 -0500

    Add vbox scripts and update site.pp for cobbler2 demo

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,11 +8,19 @@ node default {
 }
 
 node 'shoemaker.local' inherits default {
-        include screen
         include cobbler::full
         include cobbler::centos
 	sysctl::conf { "net.ipv4.ip_forward":  value => "1" }
 	cobbler::system { "node01": ip => "10.0.0.201", mac => "aa:aa:bb:bb:ff:01" }
 	cobbler::system { "node02": ip => "10.0.0.202", mac => "aa:aa:bb:bb:ff:02" }
 	cobbler::system { "node03": ip => "10.0.0.203", mac => "aa:aa:bb:bb:ff:03" }
+	cobbler::system { "cobbler2": ip => "10.0.0.204", mac => "aa:aa:bb:bb:ff:04" }
 }
+
+node 'cobbler2' inherits default {
+        include cobbler::full
+        include cobbler::centos
+	sysctl::conf { "net.ipv4.ip_forward":  value => "1" }
+	cobbler::system { "node01": ip => "10.0.0.201", mac => "aa:aa:bb:bb:ff:01" }
+}
+	
